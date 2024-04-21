2409bf87423e1f5faaabcf47103cc47d509bb3c5
mohammed <mohammedsuhail95145@gmail.com>
2024-04-21 10:17:24 +0530

    modified with some responsive design and added Simple DOM

---
+++ app.js
@@ -0,0 +1,8 @@
+
+function welcome(){
+    let tittle = document.getElementById('heading');
+    tittle.innerHTML = 'Welcome to Netflix <span id="name">Suhail!</span>'; 
+    tittle.style.color = "rgb(229, 9, 20)";
+    document.getElementById('name').style.color = "white";
+   
+}
